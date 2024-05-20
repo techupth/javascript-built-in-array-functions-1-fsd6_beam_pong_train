a08b167e47d16486d5fcb8889a599973e91acaf1
Jakkaphob <jkpbeam@gmail.com>
2024-05-20 11:54:18 +0700

    feat: add javascript built-in-array as feature/beam, might wanna fix the logic sometime after this though

--- ex-2.js
+++ ex-2.js
@@ -1,9 +1,9 @@
 let words = ["apple", "cat", "dog", "elephant"];
 
 function get5CharWords(words) {
-  // Start coding here
+  return words.length >= 5;
 }
 
-const result = get5CharWords(words);
+const result = words.filter(get5CharWords);
 
 console.log(result); // Output: ["apple", "elephant"]
